d5f9917ace2a5f06d2e4e068872cf8d76529e725
Valentine Elum <valentineelum@gmail.com>
2020-07-25 23:58:18 +0100

    Added my files

---
+++ precache-manifest.86b43171d3c9cc2bb9d58214a8f1bfd5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d2a1cfd6e6272e308638b6b273f1d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a66be31c7a479cc24d",
+    "url": "/static/css/main.ca8c1144.chunk.css"
+  },
+  {
+    "revision": "2d153a17a3c8c5af9e73",
+    "url": "/static/js/2.f623943a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f623943a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a66be31c7a479cc24d",
+    "url": "/static/js/main.206d5ee6.chunk.js"
+  },
+  {
+    "revision": "d313b7b53fbecca45471",
+    "url": "/static/js/runtime-main.3b949a3c.js"
+  },
+  {
+    "revision": "46b913b4322acaf1d9caf1511f289702",
+    "url": "/static/media/AIL.46b913b4.png"
+  },
+  {
+    "revision": "d6ccbd9a95c2d20cd75e694231055f9f",
+    "url": "/static/media/DAVIS.d6ccbd9a.jpg"
+  },
+  {
+    "revision": "ee5d921044c54daa2bac0cf25d4437ba",
+    "url": "/static/media/DNShades.ee5d9210.png"
+  },
+  {
+    "revision": "3471bf8cd0bca2fc1c081926f88b963b",
+    "url": "/static/media/Desyn.3471bf8c.png"
+  },
+  {
+    "revision": "ed96ca38eb4c49dfdc3d1a281f4e8548",
+    "url": "/static/media/IAI.ed96ca38.png"
+  },
+  {
+    "revision": "7601f75f8168fc08c8d6552753acbc2d",
+    "url": "/static/media/Phronexix.7601f75f.png"
+  },
+  {
+    "revision": "fa32f7fe0f5034a7a723133b9d0dffec",
+    "url": "/static/media/Portfolio.fa32f7fe.png"
+  },
+  {
+    "revision": "8bd50682f472337c8e7906a1078b2e34",
+    "url": "/static/media/TSN.8bd50682.png"
+  },
+  {
+    "revision": "caf7109033ecf1f60f18e4a4c30c72fe",
+    "url": "/static/media/cursor.caf71090.svg"
+  },
+  {
+    "revision": "53d3c12117d623a1188082e6c6d4cd1c",
+    "url": "/static/media/desyn-website.53d3c121.png"
+  },
+  {
+    "revision": "7083b8c447fec0ed84d30356bd7a9e50",
+    "url": "/static/media/icomoon.7083b8c4.svg"
+  },
+  {
+    "revision": "8fceb3abcf91afdd5ec12994d7f4165c",
+    "url": "/static/media/icomoon.8fceb3ab.eot"
+  },
+  {
+    "revision": "c74d019a3f19b1559d77c5120c7d9903",
+    "url": "/static/media/icomoon.c74d019a.ttf"
+  },
+  {
+    "revision": "f0ee518602a18f139c8464d257c18063",
+    "url": "/static/media/icomoon.f0ee5186.woff"
+  },
+  {
+    "revision": "69f28077578e082c471e41eef6f97d25",
+    "url": "/static/media/pattern.69f28077.svg"
+  },
+  {
+    "revision": "a2956235d3db212eb0251ce1c2ceea0e",
+    "url": "/static/media/pp.a2956235.jpg"
+  },
+  {
+    "revision": "43c90b0004314f952e8879c33aa497d7",
+    "url": "/static/media/rocc-website.43c90b00.png"
+  },
+  {
+    "revision": "9a2dbcb29668107fce771f5c82c19064",
+    "url": "/static/media/rocc.9a2dbcb2.jpeg"
+  },
+  {
+    "revision": "eb0e13bb7ddab26bdbaf17f01c81e8a9",
+    "url": "/static/media/sftv-website.eb0e13bb.png"
+  },
+  {
+    "revision": "2f640055d7b0d57991c40ebbb3c8f3b5",
+    "url": "/static/media/tsn-website.2f640055.png"
+  }
+]);
